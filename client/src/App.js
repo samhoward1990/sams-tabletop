e808f77f12712feb798f5b1f90a73a1b9d8d5dd2
Sam Howard <samhoward1990@gmail.com>
2020-10-01 14:15:46 -0500

    remade logo as text with custom styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import { Navbar, Nav, Image, Button } from 'react-bootstrap';
 import './App.css';
 
-import rpLogo from './images/rp-logo.png';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar bg="light" expand="lg">
-          <Link to="/"><Navbar.Brand><Image src={rpLogo} width="50px" height="50px" /></Navbar.Brand></Link>
+          <Link to="/"><Navbar.Brand className="styled-text">RP</Navbar.Brand></Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
